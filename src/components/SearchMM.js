49fc99c768746fed21f0ce0dced8ea39680a864f
Jesse McKinney <jessman51386@gmail.com>
2020-04-23 18:24:27 -0400

    yay

---
+++ src/components/SearchMM.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./SearchMM.css";
+
+class SearchMM extends Component {
+  render() {
+    return (
+      <>
+        <div></div>
+      </>
+    );
+  }
+}
+
+export default SearchMM;
