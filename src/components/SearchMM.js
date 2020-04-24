024289a2f041a40ef6f6094074dc62f4fe393551
Jesse McKinney <jessman51386@gmail.com>
2020-04-23 22:35:49 -0400

    I've got all results coming back as long as the lyrics are at least 10 words perfect like the actual lyrics in the artists tracks are matched up, anything not spelled right will result in error

--- src/components/SearchMM.js
+++ src/components/SearchMM.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import "./SearchMM.css";
 require("dotenv").config();
+
+const API_KEY = "";
 
 const MUSIX_API_ROOT = "https://api.musixmatch.com/ws/1.1/";
 
 var words =
-  "Blinded%20by%20the%20light%20Revved%20up%20like%20a%20deuce%20Another%20runner%20in%20the%20night";
+  "Blinded%20by%20the%20light%20Revved%20up%20like%20a%20deuce%20Another%20runner%20in%20the%20night"; //Michael Mind Project
+// "I%20wanna%20dance%20with%20somebody%20I%20wanna%20feel%20the%20heat%20with%20somebody"; //Whitney Houston
+// "As%20I%20went%20down%20to%20the%20river%20to%20pray%20studying%20about%20that%20good%20ol%20way"; //Allison Krauss
+// "i%20was%20a%20liar%20i%20gave%20into%20the%20fire%20i%20know%20i%20should've%20fought%20it%20at%20least%20i'm%20being%20honest"; //Arianna Grande
 
 class SearchMM extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      // words: "",
+    };
     this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   componentDidMount() {
@@ -26,16 +35,25 @@ class SearchMM extends Component {
       MUSIX_API_ROOT +
       "track.search?q_lyrics=" +
       words +
-      "&page_size=10&page=1&s_track_rating=desc&apikey=" +
-      process.env.API_KEY;
+      "&page_size=10&page=1&s_track_rating=desc&apikey=" + API_KEY;
     console.log(MUSIX_API_URL);
   }
 
   render() {
     return (
       <>
-        <div>You Need At Least 10 Words To Match To Make It Work!!!</div>
-        <button onClick={this.onFormSubmit}>Click Here</button>
+        <div>You Need At Least 10 Words To Match !!!EXACTLY!!! To Make It Work!!!</div>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Enter Lyrics</Form.Label>
+            <Form.Control type="text" placeholder="Lyrics Go Here">
+              {this.words}
+            </Form.Control>
+            <Button variant="primary" type="submit" onClick={this.onFormSubmit}>
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
       </>
     );
   }
