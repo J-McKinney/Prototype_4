1fde80f9514c58913d1315395066150a80292385
Jesse McKinney <jessman51386@gmail.com>
2020-04-25 14:26:17 -0400

    yay

--- src/components/SearchMM.js
+++ src/components/SearchMM.js
@@ -8,8 +8,8 @@ const API_KEY = "";
 
 const MUSIX_API_ROOT = "https://api.musixmatch.com/ws/1.1/";
 
-var lyric = 
-"Blinded by the light Revved up like a deuce Another runner in the night";
+// var lyric =
+// "Blinded by the light Revved up like a deuce Another runner in the night";
 // "Blinded%20by%20the%20light%20Revved%20up%20like%20a%20deuce%20Another%20runner%20in%20the%20night"; //Michael Mind Project
 // "I%20wanna%20dance%20with%20somebody%20I%20wanna%20feel%20the%20heat%20with%20somebody"; //Whitney Houston
 // "As%20I%20went%20down%20to%20the%20river%20to%20pray%20studying%20about%20that%20good%20ol%20way"; //Allison Krauss
@@ -20,62 +20,58 @@ class SearchMM extends Component {
     super(props);
     this.state = {
       lyrics: "",
+      form: "",
     };
 
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    // this.changeFunction = this.changeFunction.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-
   componentDidMount() {
-    console.log("componentDidMount: " + this.state.lyrics);
+    console.log("componentDidMount:lyrics " + this.state.lyrics);
+    console.log("componentDidMount:form " + this.state.form);
   }
   componentDidUpdate() {
     // this.music()
-    console.log("componentDidUpdate: " + this.state.lyrics);
+    console.log("componentDidUpdate:lyrics " + this.state.lyrics);
+    console.log("componentDidUpdate:form " + this.state.form);
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount:lyrics " + this.state.lyrics);
+    console.log("componentWillUnmount:form " + this.state.form);
   }
 
-  handleInputChange = (event) => {
-    const name = event.target.name;
-    let value = event.target.value;
+  handleChange = (e) =>
     this.setState({
-      [name]: value,
+      lyrics: e.target.value,
+      form: e.target.value,
     });
-  };
 
-  // changeFunction(event) {
-  //   event.preventDefault();
-  //   this.setState({ lyrics: this.state.lyrics.split(" ").join("%20")});
-  //   console.log("changeFunction: " + this.state.lyrics);
-  //   // this.onFormSubmit()
-  // }
-
-  onFormSubmit(event) {
-    event.preventDefault();
-    // this.setState({ lyrics: this.state.lyrics.split(" ").join("%20")});
-
-    // this.changeFunction()
-    // this.music()
-
-    // var separateToIndividualStrings = this.state.lyrics.split(" ");
-    // var joinSeparatedWords = separateToIndividualStrings.join("%20");
-    // this.setState({ lyrics: joinSeparatedWords });
-
-    console.log("onFormSubmit: " + this.state.lyrics);
-
-    lyric = lyric.split(" ").join("%20");
-
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("handleSubmit lyrics: " + this.state.lyrics);
+    console.log("handleSubmit form: " + this.state.form);
     const MUSIX_API_URL =
       MUSIX_API_ROOT +
       "track.search?q_lyrics=" +
       // joinSeparatedWords +
       // this.state.spokenWords +
       // this.state.lyrics +
-      lyric +
+      this.state.form +
+      // lyric +
       // this.setState({ lyrics: this.state.lyrics.split(" ").join("%20")}) +
       "&page_size=1&page=1&s_track_rating=desc&apikey=" +
       API_KEY;
     console.log("MusicAPI: " + MUSIX_API_URL);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    //   this.setState({ lyrics: this.state.lyrics.split(" ").join("%20")});
+    // this.setState({ form: this.state.lyrics.split(" ").join("%20") });
+    console.log("onFormSubmit:lyrics " + this.state.lyrics);
+    console.log("componentDidUpdate:form " + this.state.form);
+
     // Clear the input field
     // this.setState({
     //   lyrics: "",
@@ -83,24 +79,29 @@ class SearchMM extends Component {
   }
 
   render() {
+    const { state } = this;
     return (
       <>
         <div>
           &nbsp; You Need At Least 10 Words To Match !!!EXACTLY!!! To Make It
           Work. &nbsp;
         </div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="lyricForm">
             <Form.Label>&nbsp; Enter Lyrics &nbsp;</Form.Label>
             <Form.Control
               type="text"
               placeholder="Lyrics Go Here"
-              name="lyrics"
               // value={this.spokenWords}
-              onChange={this.handleInputChange}
-              value={this.state.lyrics}
+              onChange={(e) =>
+                this.setState({
+                  lyrics: e.target.value,
+                  form: this.state.lyrics.split(" ").join("%20"),
+                })
+              }
+              value={state.lyrics}
             ></Form.Control>
-            <Button variant="primary" type="submit" onClick={this.onFormSubmit}>
+            <Button type="submit" value="Submit">
               Submit
             </Button>
           </Form.Group>
